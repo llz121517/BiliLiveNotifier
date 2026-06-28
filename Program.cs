b8e23dc2fc2a3347f8bd875290fb2e48cf6f8080
llz12 <llz1234562022@outlook.com>
2026-06-28 22:30:18 +0800

    重构主流程，支持配置化多UID轮询与通知
    重构 Program.cs，拆分 Main 与 MonitorService，支持从配置读取 UIDs、轮询间隔等参数，实现对直播间状态的定时检测与去重通知，移除硬编码，优化日志输出。

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ namespace BiliLiveNotifier;
 
 class Program
 {
+    public static int Interval { get; set; }
+    public static bool IsToast { get; set; } = false;
     static async Task Main(string[] args)
     {
         LLog.Level = LLog.LogLevel.Debug;
@@ -19,34 +21,56 @@ class Program
             return;
         }
 
-        // 1. 先加载配置并查询真实房间号
-        long uid = 496751305;
+        // 1. Init
+        ConfigManager.Initialize();
         ApiClient.LoadEndpoints();
 
-        var data = await ApiClient.RequestAsync("GetMasterInfo", uid);
+        var cfg = ConfigManager.Config;
+        long[] uids = cfg?["Uids"]?.AsArray()
+            .Select(v => (long)v!)
+            .ToArray() ?? [];
+        Interval = cfg?["check_interval"]?.GetValue<int>() ?? 15000;
+        bool autoStart = cfg?["auto_start"]?.GetValue<bool>() ?? false;
+
+        await Program.MonitorService(uids[0]);
+
+        ToastImageCache.ClearCache();
+    }
+    static async Task MonitorService(long Uid)
+    {
+        var data = await ApiClient.RequestAsync("GetMasterInfo", Uid);
         long? roomId = data?.GetPath("room_id")?.GetValue<long>();
 
         if (!roomId.HasValue)
         {
-            LLog.Warn($"[Init] UID:{uid} 解析直播间ID失败, 跳过通知发送");
+            LLog.Warn($"[Init] UID:{Uid} 解析直播间ID失败, 跳过通知发送");
             return;
         }
 
-        LLog.Info($"[Init] UID:{uid} 解析到直播间ID: {roomId}");
+        LLog.Info($"[Init] UID:{Uid} 解析到直播间ID: {roomId}");
 
-        // 业务层负责协调，Notifier 只接收 Uri
-        var avatarUri = await ToastImageCache.GetLocalUriAsync("https://i1.hdslb.com/bfs/face/4c6e413e3789ef1bfaa738b05db977f4d7129858.jpg");
-        var coverUri = await ToastImageCache.GetLocalUriAsync("https://i0.hdslb.com/bfs/live/new_room_cover/d57ca8a633b288333c1f0bb260a55ea46dca882b.jpg");
+        while (true)
+        {
+            data = await ApiClient.RequestAsync("GetLiveRoomDetail", roomId);
+            long? live_status = data?.GetPath("live_status")?.GetValue<long>();
 
-        // 2. 使用真实房间号拼接 URL 并发送中文通知
-        string liveUrl = $"https://live.bilibili.com/{roomId}";
+            if (live_status == 1 || live_status == 2 && IsToast == true)
+            {
+                // 业务层负责协调，Notifier 只接收 Uri
+                var avatarUri = await ToastImageCache.GetLocalUriAsync("https://i1.hdslb.com/bfs/face/4c6e413e3789ef1bfaa738b05db977f4d7129858.jpg");
+                var coverUri = await ToastImageCache.GetLocalUriAsync("https://i0.hdslb.com/bfs/live/new_room_cover/d57ca8a633b288333c1f0bb260a55ea46dca882b.jpg");
 
-        await ToastNotifier.SendLiveNotificationAsync("headerTitle", "title", "subtitle", coverUri, avatarUri, liveUrl);
+                // 2. 使用真实房间号拼接 URL 并发送通知
+                string liveUrl = $"https://live.bilibili.com/{roomId}";
 
-        await Task.Delay(3000);
-
-        ToastImageCache.ClearCache();
-
-        await ApiClientTester.RunTestAsync();
+                await ToastNotifier.SendLiveNotificationAsync("headerTitle", "title", "subtitle", coverUri, avatarUri, liveUrl);
+                IsToast = true;
+            }
+            if (live_status == 0)
+            {
+                IsToast = false;
+            }
+            await Task.Delay(Interval);
+        }
     }
 }
